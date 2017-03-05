16dc1ef6def59ee3354daf6cd0b8fb30accfeafb
Sara Ford (Tips) <saraford-tips@users.noreply.github.com>
2017-03-05 15:11:22 -0800

    added Read() to keep console window from closing

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace SimpleConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Console App World");
+            Console.Read();
         }
     }
 }
